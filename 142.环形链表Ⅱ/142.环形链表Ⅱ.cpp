cecb99ecf1b9be765a0bdba8e52cfddcf0f5381b
2580553038 <2580553038@qq.com>
2026-01-13 15:24:47 +0800

    完成了一道链表题目

--- 142.环形链表Ⅱ/142.环形链表Ⅱ.cpp
+++ 142.环形链表Ⅱ/142.环形链表Ⅱ.cpp
@@ -1,11 +1,60 @@
-﻿// 142.环形链表Ⅱ.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+// 142.环形链表Ⅱ.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
 
 #include <iostream>
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* kuai=head;
+        ListNode* man=head;
+
+        
+        while(true)
+        {
+            //1.是否有环
+            if(kuai==NULL||kuai->next==NULL)
+            {
+                return NULL;
+            }
+            kuai=kuai->next->next;
+            man=man->next;
+            //2.是否相遇
+            if(kuai==man)
+            {
+                break;
+            }
+        }
+        //3.寻找入环点
+        kuai=head;
+        while(kuai!=man)
+        {
+            kuai=kuai->next;
+            man=man->next;
+        }
+        return kuai;
+    }
+};
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    return 1;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
